3b057a797414140460482c812fb3a7fcc3017f93
Carlos Mullov <mullovc@hotmail.de>
2021-02-19 11:53:24 +0100

    add function to train from checkpoint (while using a new vocabulary)

--- functions/train.sh
+++ functions/train.sh
@@ -16,3 +16,30 @@ train() {
         -config "$configdir/$trainconfig.yml" 2>&1 |
             tee "$logdir/training.$stage.$model.log"
 }
+
+train_continue() {
+    # TODO add `${additional_args}` array to `train()` and call `train` here
+    # TODO move some of this into run.sh?
+    local stage="$1"
+    local model="$2"
+    local trainfrom="$3"
+    local trainconfig="$4"
+
+    local savedir="$projectroot/saves.$stage"
+    local modeldir="$savedir/models/$model"
+
+    mkdir -p "$modeldir"
+
+    # NOTE instead of using `-pre_word_vecs_enc "$savedir/embeddings.tgt.pt"`
+    # let onmt load the embedding vectors from tgt_vocab.vectors
+    python -u "$onmt"/train.py \
+        -data "$savedir/data" \
+        -save_model "$modeldir/$model" \
+        -train_from "$trainfrom" \
+        -reset_optim all \
+        -modify_opts \
+        -gpu_ranks 0 \
+        -new_vocab "$savedir/data.vocab.pt" \
+        -config "$configdir/$trainconfig.yml" 2>&1 |
+            tee "$logdir/training.$stage.$model.log"
+}
