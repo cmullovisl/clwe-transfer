240a2cf9ca4bd1597281510bcc029b0c4d5c7f08
Carlos Mullov <mullovc@hotmail.de>
2021-02-23 10:04:17 +0100

    fix some issues with download and alignment of word embeddings

--- functions/make-embeddings.sh
+++ functions/make-embeddings.sh
@@ -1,19 +1,23 @@
 download_fasttext_model() {
     local lng="$1"
     local fasttext_url="https://dl.fbaipublicfiles.com/fasttext/vectors-crawl"
+    local nvectors=200000
 
-    # TODO limit amount of vocab words downloaded
-    curl "${fasttext_url}/cc.${lng}.${embdim}.bin.gz" > "$embeddingsdir/cc.${lng}.${embdim}.bin.gz"
-    curl "${fasttext_url}/cc.${lng}.${embdim}.vec.gz" > "$embeddingsdir/cc.${lng}.${embdim}.vec.gz"
-    # TODO decompress
+    curl "${fasttext_url}/cc.${lng}.${embdim}.bin.gz" |
+        gunzip > "$embeddingsdir/cc.${lng}.${embdim}.bin"
+
+    # Download only the first `$nvectors` word vectors, since 2.000.000 vectors
+    # is an excessive amount. Use `|| true` to suppress curl error as to not
+    # crash the whole script due to Bash `pipefail` option.
+    curl "${fasttext_url}/cc.${lng}.${embdim}.vec.gz" |
+        gunzip | sed "1s/^[1-9][0-9]*/${nvectors}/" |
+        head -n "$((nvectors+1))" > "$embeddingsdir/cc.${lng}.${embdim}.vec" || true
 }
 
 download_embeddings() {
     local lng
     for lng in "$@"; do
-        [[ -f ${embeddingsdir}/cc.${lng}.${embdim}.bin.gz ]] || download_fasttext_model "$lng"
-        # TODO copy dictionaries for language pairs
-        [[ -f ${embeddingsdir}/${lng}-*.txt ]] || download_dictionaries "$lng"
+        [[ -f ${embeddingsdir}/cc.${lng}.${embdim}.bin ]] || download_fasttext_model "$lng"
     done
 }
 
@@ -23,19 +27,28 @@ compute_alignments() {
     local pivot="$1"
     shift
 
+    # TODO copy dictionaries for language pairs
+    [[ -e $dictdir ]] || download_dictionaries
+
     for lng in "$@"; do
+
         [[ $pivot = "$lng" ]] && continue
 
+        # fasttext saves the aligned embeddings to the output file (`--output`)
+        # and the alignment matrix to the output file + "-mat" suffix. Since we
+        # only require the matrix, we discard the embeddings by writing to
+        # /dev/null (through a symbolic link hack)
+        outfile="$embeddingsdir/aligned.$lng.vec"
+        ln -s /dev/null "$outfile"
+
         python -u "$fasttext"/alignment/align.py \
-            --src_emb "$embeddingsdir/cc.$lng.300.vec" \
-            --tgt_emb "$embeddingsdir/cc.$pivot.300.vec" \
+            --src_emb "$embeddingsdir/cc.$lng.$embdim.vec" \
+            --tgt_emb "$embeddingsdir/cc.$pivot.$embdim.vec" \
             --dico_train "$dictdir/$lng-$pivot.txt" \
             --dico_test  "$dictdir/$lng-$pivot.0-5000.txt" \
-            --output "$embeddingsdir/aligned.$lng.vec" 2>&1 | tee "$logdir/align.$lng.log"
-
-        #[[ $deleteftvec ]] && rm -f "$embeddingsdir/cc.${lng}.${embdim}.vec.gz"
+            --output "$outfile" 2>&1 | tee "$logdir/align.$lng.log"
     done
 
-    # XXX
-    # "compute" dummy alignment for pivot?
+    python "$SCRIPT_DIR"/scripts/fasttext/write-eye.py \
+        "$embdim" > "$embeddingsdir/aligned.$pivot.vec-mat"
 }
