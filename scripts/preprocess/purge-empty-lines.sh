f9aedd0aa7e4c1e3296e587af7be40b2488d36df
Carlos Mullov <carlos.mullov@kit.edu>
2023-11-29 17:40:02 +0100

    finish moving scripts to /bin and delete obsolete /scripts directory

--- scripts/preprocess/purge-empty-lines.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-MARKER=$1
-SRC=$2
-TGT=$3
-
-sed -i -s "$(grep -n "^$MARKER$" $SRC | cut -d: -f1 | tr '\n' ' ' | sed 's/ /d;/g')" $SRC $TGT
-sed -i -s "$(grep -n "^$MARKER$" $TGT | cut -d: -f1 | tr '\n' ' ' | sed 's/ /d;/g')" $SRC $TGT
