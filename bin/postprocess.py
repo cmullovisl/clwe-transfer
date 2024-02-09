6df70e9641d49f483a68b03b311bff4ce25fe33a
Carlos Mullov <carlos.mullov@kit.edu>
2024-02-09 12:47:07 +0100

    postprocess hypotheses with many n-gram repititions more aggressively
    Keep track of the ratio of unclean lines (i.e. lines with n-gram
    repititions) during postprocessing. Based on the ratio we then filter
    after 2 repititions already, instead of waiting for 3.

--- bin/postprocess.py
+++ bin/postprocess.py
@@ -2,28 +2,33 @@
 import sys
 import re
 
-#for line in sys.stdin:
-#    # replace duplicates at end of sentence with "."
-#    ppline = re.sub("([^ ]+)( \\1)+$", "\\1 .", line.rstrip())
-#    # remove all other duplicates
-#    ppline = re.sub("([^ ]+)( \\1)+", "\\1", ppline)
-#
-#    print(ppline)
-
 
 langcode = sys.argv[1]
 
-for line in sys.stdin:
+aggressive = False
+unclean_count = 0
+for i, line in enumerate(sys.stdin):
     ppline = line.rstrip()
+
+    if (re.search("{0}@([^ ]+) {0}@\\1( {0}@\\1)+".format(langcode), ppline)
+            or re.search("(({0}@[^ ]+ {0}@[^ ]+)(?: {0}@[^ ]+)*?) \\1( \\1)+".format(langcode), ppline)):
+        unclean_count += 1
+    aggressive = unclean_count / float(i + 1) > 0.1
+
     # replace duplicates at end of sentence with "."
     ppline = re.sub("{0}@([^ ]+)( {0}@\\1)+$".format(langcode), "\\1 .", ppline)
     # remove all other duplicates
-    #ppline = re.sub("{0}@([^ ]+)( {0}@\\1)+".format(langcode), "\\1", ppline)
-    ppline = re.sub("{0}@([^ ]+) {0}@\\1( {0}@\\1)+".format(langcode), "\\1", ppline)
+    if aggressive:
+        ppline = re.sub("{0}@([^ ]+)( {0}@\\1)+".format(langcode), "\\1", ppline)
+    else:
+        ppline = re.sub("{0}@([^ ]+) {0}@\\1( {0}@\\1)+".format(langcode), "\\1", ppline)
     # remove duplicates consisting of two words
     #ppline = re.sub("{0}@([^ ]+) {0}@([^ ]+)( {0}@\\1 {0}@\\2)+".format(langcode), "\\1 \\2", ppline)
     # remove instances of 3+ repititions of n-grams (for n >= 2)
-    ppline = re.sub("(({0}@[^ ]+ {0}@[^ ]+)(?: {0}@[^ ]+)*?) \\1( \\1)+".format(langcode), "\\1", ppline)
+    if aggressive:
+        ppline = re.sub("(({0}@[^ ]+ {0}@[^ ]+)(?: {0}@[^ ]+)*?)( \\1)+".format(langcode), "\\1", ppline)
+    else:
+        ppline = re.sub("(({0}@[^ ]+ {0}@[^ ]+)(?: {0}@[^ ]+)*?) \\1( \\1)+".format(langcode), "\\1", ppline)
     # remove language codes
     ppline = re.sub("{0}@".format(langcode), "", ppline)
 
