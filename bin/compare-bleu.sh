c4a7fc7ae31a8b96df1c6470b35aaae96d60111f
Carlos Mullov <carlos.mullov@kit.edu>
2024-02-09 12:41:56 +0100

    add script to compare scores between different stages

--- bin/compare-bleu.sh
+++ bin/compare-bleu.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
-##paste <(
-##    ls -1d "$dir"
-##    ls -1d "$dir"/??-??
-#paste <(
-#    ls -1d ??-??
-#) <(
-#    jq '.[1].score' "$1"/??-??/"$scorefile"
-#) <(
-#    jq '.[1].score' "$2"/??-??/"$scorefile"
-#) | perl -lane 'printf "%s\t%+.1f\n", $_, $F[2]-$F[1]'
-
-
 join <(
-    jq -r '"\(input_filename) \(.score)"' "$1"/*.??-??.jsonl |
-        sed 's#\([^ ]*/\|^\)\([^ /]*\)\.\([a-z][a-z]-[a-z][a-z]\)\.jsonl #\2.\3 #' |
+    jq -r '"\(input_filename) \(.[0].score)"' "$1"/*.??-??.jsonl |
+        sed 's#\([^ ]*/\|^\)\([^ /]*\)\.\([a-z][a-z]-[a-z][a-z]\)\.jsonl #\3.\2 #' |
         sort
 ) <(
-    jq -r '"\(input_filename) \(.score)"' "$2"/*.??-??.jsonl |
-        sed 's#\([^ ]*/\|^\)\([^ /]*\)\.\([a-z][a-z]-[a-z][a-z]\)\.jsonl #\2.\3 #' |
+    jq -r '"\(input_filename) \(.[0].score)"' "$2"/*.??-??.jsonl |
+        sed 's#\([^ ]*/\|^\)\([^ /]*\)\.\([a-z][a-z]-[a-z][a-z]\)\.jsonl #\3.\2 #' |
         sort
 ) |
-    perl -lane 'printf "%s\t%+.1f\n", $_, $F[2]-$F[1]' |
-    column -t
+    perl -lane '
+        $avg += $F[2]-$F[1];
+        printf "%s\t%+.1f\n", $_, $F[2]-$F[1];
+        END { printf "xx-xx.avg   %+.2f\n", $avg/$.; }' |
+    sed 's/\./ /' |
+    column -t -n
